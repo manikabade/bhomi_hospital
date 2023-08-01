7861993b6979ac4aa32947e21667639954660b61
javaccoding <srabeenaa098@gmail.com>
2023-08-01 18:10:05 +0545

    frontend added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin\Doctor;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +12,7 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
@@ -23,6 +21,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $data = [] ;
+        $data['_doctors'] = Doctor::active()->get();
+
+        return view('welcome',compact('data') );
     }
 }
